5b6704f746c9aff531b745cc0a9bdfa91030d074
jacob-kourieh <you@example.com>
2022-04-21 21:01:13 +0200

    #3 Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <header>
-
+            <nav>
+                <Link to="/">
+                    <h1>Agila Metoder</h1>
+                    <p>Scrum</p>
+                </Link>
+            </nav>
         </header>
     );
 }
