865e18c9f0ac9e1f351616ab493015fa08ae4d3c
jacob-kourieh <you@example.com>
2022-04-25 18:01:11 +0200

    #10 byta bild-flytta text

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -2,45 +2,12 @@ import { Link } from "react-router-dom";
 
 function Start() {
 
-    let img = "https://www.visual-paradigm.com/servlet/editor-content/scrum/what-is-scrum-team/sites/7/2018/10/what-is-scrum-team.png"
+    let img = "https://s3.ap-southeast-2.amazonaws.com/april9.com.au/files/pvj0l1IuMXsXUklQ"
 
 
     return (
         <section className="container">
-
-            <article className="AgilaTeam">
-                <h2> Att Arbeta i Agila Team</h2>
-                <img src={img} alt="" />
-                <ul>
-                    <li>Identifiera, informera och tillsätt en Produktägare</li>
-                    <li>Organisation, team och roller</li>
-                    <li>Utveckla det självstyrande teamet</li>
-                    <li>Sätta grundregler för arbetet</li>
-                    <li>Utveckla ledarskapet</li>
-                    <li>Skala av och förenkla</li>
-                    <li>Lär ut de nya rutinerna</li>
-                    <Link to="/agila-team"> <button>mer info</button> </Link>
-                </ul>
-            </article>
-
-
-
-            <article className="scrumHeading">
-                <h2>Scrum – roller, begrepp och aktiviteter</h2>
-                <p>Scrum är ett ramverk som implementerar det Agila tankesättet.
-                    Det Agila manifestet i sig specificerar inte några direkt konkreta krokar att hänga upp sitt arbetssätt på.
-                    Däremot finns det ett antal konkretiseringar och implementationer som tillämpar den Agila filosofin, såsom Scrum, Kanban eller DevOps.
-                    En populär ingång till att få igång sitt Agila arbete är att man helt enkelt inför just Scrum som metodik, i stor eller liten skala.
-
-                    Det finns många anledningar till att Scrum blivit så populärt.
-                    En av dem är att de praktiska delar som utgör ramverket är relativt okomplexa och konkreta, vilket vi kommer att se nedan.
-
-                    Vidare är en av de stora skillnaderna från traditionell projektmetodik är att Scrums ramverk är uppbyggt på empirism.
-                    Det innebär att man tar beslut utifrån tidigare erfarenheter och information man faktiskt kan bekräfta.
-                    Inga “vi är helt säkra på att det här är det bästa sedan skivat bröd, så vi bygger allt på en gång”-projekt, alltså.
-                    Tvärtom vill vi skapa en kultur som inlemmar “vi tror att det här är en grym idé, men vi utvecklar lite i taget”.</p>
-            </article>
-
+            <img src={img} alt="" width="1000px" height="600px" />
 
 
             <article className="ScrumInfo">
@@ -56,6 +23,36 @@ function Start() {
                 </ul>
             </article>
 
+
+            <article className="scrumHeading">
+                <h2> Roller, Begrepp och Aktiviteter</h2>
+                <p> Det Agila manifestet i sig specificerar inte några direkt konkreta krokar att hänga upp sitt arbetssätt på.
+                    Däremot finns det ett antal konkretiseringar och implementationer som tillämpar den Agila filosofin, såsom Scrum, Kanban eller DevOps.
+                    En populär ingång till att få igång sitt Agila arbete är att man helt enkelt inför just Scrum som metodik, i stor eller liten skala.
+                    <br />
+                    Det finns många anledningar till att Scrum blivit så populärt.
+                    En av dem är att de praktiska delar som utgör ramverket är relativt okomplexa och konkreta.
+                    <br />
+                    Vidare är en av de stora skillnaderna från traditionell projektmetodik är att Scrums ramverk är uppbyggt på empirism.
+                    Det innebär att man tar beslut utifrån tidigare erfarenheter och information man faktiskt kan bekräfta.
+                    Inga “vi är helt säkra på att det här är det bästa sedan skivat bröd, så vi bygger allt på en gång”-projekt, alltså.
+                    Tvärtom vill vi skapa en kultur som inlemmar “vi tror att det här är en grym idé, men vi utvecklar lite i taget”.</p>
+            </article>
+
+
+            <article className="AgilaTeam">
+                <h2> Att Arbeta i Agila Team</h2>
+                <ul>
+                    <li>Identifiera, informera och tillsätt en Produktägare</li>
+                    <li>Organisation, team och roller</li>
+                    <li>Utveckla det självstyrande teamet</li>
+                    <li>Sätta grundregler för arbetet</li>
+                    <li>Utveckla ledarskapet</li>
+                    <li>Skala av och förenkla</li>
+                    <li>Lär ut de nya rutinerna</li>
+                    <Link to="/agila-team"> <button>mer info</button> </Link>
+                </ul>
+            </article>
 
 
             <article className="scrumCeremonier">
